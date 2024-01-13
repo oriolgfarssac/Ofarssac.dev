1d5c05ea723ade24b021cef80d111fea6f1b3bfa
Oriol Gonzalez Farssac <oriolgfarssac@gmail.com>
2024-01-13 16:16:40 +0100

    v1 presentation

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <div className="mainContainer">
+          <img src="src\assets\img.png" alt="pic" className='img'/>
+          <div className='nav'>
+            <a href="#aboutMe" className="sections">About Me</a>
+            <a href="" className="sections">Skills</a>
+            <a href="" className="sections">Projects</a>
+            <a href="" className="sections">Contact</a>
+          </div>
+          <div id='aboutMe'>
+          <h1 className='intro'>
+          <b>Hey there! Welcome to my portfolio! </b><br/><br/> I'm Oriol, a Front-End Developer from Barcelona. <br/> My passion lies in the art of crafting interfaces that not only catch the eye but also make user experiences seamless and enjoyable. <br/><br/> It's all about finding that sweet spot where creativity and functionality dance together in perfect harmony. If you're curious to see how this mix translates into my projects, you're in the right place.</h1>
+          <img src="src\assets\oriol.jpg" alt="myImg" id="picture"/>
+          </div>
+          <div className="presentationContainer">
+            <div className="firstPart">
+              <img src="" alt="" className="myImg" />
+              <h1 className="title" id='aboutMe'></h1>
+            </div>
+            <p className="present1"></p>
+            <p className="present2"></p>
+          </div>
+        </div>
+    </> 
+  )
+}
+
+export default App
