cb2966bec368eabb907ee495723dcee86f564505
Oriol Gonzalez Farssac <oriolgfarssac@gmail.com>
2024-01-13 16:23:57 +0100

    more little fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
         <div className="mainContainer">
-          <img src="src/assets/img.png" alt="pic" className='img'/>
+          <img src={"src/assets/img.png"} alt="pic" className='img'/>
           <div className='nav'>
             <a href="#aboutMe" className="sections">About Me</a>
             <a href="" className="sections">Skills</a>
@@ -16,7 +16,7 @@ function App() {
           <div id='aboutMe'>
           <h1 className='intro'>
           <b>Hey there! Welcome to my portfolio! </b><br/><br/> I'm Oriol, a Front-End Developer from Barcelona. <br/> My passion lies in the art of crafting interfaces that not only catch the eye but also make user experiences seamless and enjoyable. <br/><br/> It's all about finding that sweet spot where creativity and functionality dance together in perfect harmony. If you're curious to see how this mix translates into my projects, you're in the right place.</h1>
-          <img src="src/assets/oriol.jpg" alt="myImg" id="picture"/>
+          <img src={"src/assets/oriol.jpg"} alt="myImg" id="picture"/>
           </div>
           <div className="presentationContainer">
             <div className="firstPart">
