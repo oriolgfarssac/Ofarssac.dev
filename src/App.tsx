3e1b1b9f61b969adc6c6f3ca2d24cdfc08c4b1e3
Oriol Gonzalez Farssac <oriolgfarssac@gmail.com>
2024-01-13 17:45:38 +0100

    more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import logo from "./assets/img.png"
 import yo from "./assets/oriol.jpg"
+import github from "./assets/githubIcon.svg"
+import linkedin from "./assets/linkedin.png"
 function App() {
 
 
@@ -16,7 +18,7 @@ function App() {
           </div>
           <div id='aboutMe'>
           <h1 className='intro'>
-          <b>Hey there! Welcome to my portfolio! </b><br/><br/> I'm Oriol, a Front-End Developer from Barcelona. <br/> My passion lies in the art of crafting interfaces that not only catch the eye but also make user experiences seamless and enjoyable. <br/><br/> It's all about finding that sweet spot where creativity and functionality dance together in perfect harmony. If you're curious to see how this mix translates into my projects, you're in the right place.</h1>
+          <b>Hey there! Welcome to my portfolio! </b><br/><br/> I'm Oriol, a Front-End Developer from Barcelona. <br/> My passion lies in the art of crafting interfaces that not only catch the eye but also make user experiences seamless and enjoyable. <br/><br/> It's all about finding that sweet spot where creativity and functionality dance together in perfect harmony. <br/> If you're curious to see how this mix translates into my projects, you're in the right place.  <br/><br/> <a href="https://github.com/oriolgfarssac" id='icon'><img src={github} alt="" id='iconImg' /></a>  <a href="https://www.linkedin.com/in/oriol-g-farssac/" id='icon'><img src={linkedin} alt="" id='iconImg' /></a></h1>
           <img src={yo} alt="myImg" id="picture"/>
           </div>
           <div className="presentationContainer">
